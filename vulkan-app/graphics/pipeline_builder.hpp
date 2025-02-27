aad7282d554aec10e3036f8717dcd07717376ff9
Al Morgan <al.reuben.morgan@gmail.com>
2025-02-26 19:37:47 -0800

    Add more pipeline code

--- vulkan-app/graphics/pipeline_builder.hpp
+++ vulkan-app/graphics/pipeline_builder.hpp
@@ -12,8 +12,16 @@ class pipeline_builder
 public:
     pipeline_builder(graphics::context& context);
     ~pipeline_builder();
+    void reset();
+    VkPipeline get_result();
+    void set_layout(VkPipelineLayout layout);
+    void set_vertex_shader(VkShaderModule shader);
+    void set_fragment_shader(VkShaderModule shader);
 private:
     graphics::context& m_context;
+    VkPipelineLayout m_layout;
+    VkShaderModule  m_vertex_shader;
+    VkShaderModule m_fragment_shader;
 };
 
 }
