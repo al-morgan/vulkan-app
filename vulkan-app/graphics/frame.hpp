0e722beda8b853fc164beaaf0148dac3fcaa4cf9
Al Morgan <al.reuben.morgan@gmail.com>
2025-03-02 19:59:05 -0800

    Remove command buffer from frame

--- vulkan-app/graphics/frame.hpp
+++ vulkan-app/graphics/frame.hpp
@@ -21,27 +21,11 @@ private:
     graphics::canvas& m_context;
 public:
     graphics::buffer ubuffer;
-    VkCommandPool m_command_pool;
-    VkCommandBuffer m_command_buffer;
 
     frame(graphics::canvas& context) :
         m_context(context),
         ubuffer(context, sizeof(mvp), VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT)
     {
-        VkCommandPoolCreateInfo create_info{};
-        create_info.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
-        create_info.flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT;
-        create_info.queueFamilyIndex = context.graphics_queue.family_index;
-
-        check(vkCreateCommandPool(m_context.m_device, &create_info, nullptr, &m_command_pool));
-
-        VkCommandBufferAllocateInfo alloc_info{};
-        alloc_info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
-        alloc_info.commandBufferCount = 1;
-        alloc_info.commandPool = m_command_pool;
-        alloc_info.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
-
-        vkAllocateCommandBuffers(m_context.m_device, &alloc_info, &m_command_buffer);
     }
 
     frame(const graphics::frame& other) :
@@ -50,10 +34,6 @@ public:
 
     }
 
-    ~frame()
-    {
-        vkDestroyCommandPool(m_context.m_device, m_command_pool, nullptr);
-    }
 };
 
 }
